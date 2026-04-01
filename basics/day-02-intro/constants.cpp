2a30ae9edecff60f2f0ab106866618a5ce75ffdf
Goutham R <gouthamkumar938@gmail.com>
2026-04-01 18:45:28 +0530

    feat: Added examples for constants ans initialization

---
+++ basics/day-02-intro/constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX_USERS = 100; // here const is used so it will not allow the compiler to alter it keeps same value till the end of program.
+
+    cout << "Max Users: " << MAX_USERS << endl;// simply constant is function is used to maintain same thing no changes.
+
+    return 0;
+}
---
+++ basics/day-02-intro/datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    double c = 3.1415926535;
+    char d = 'G';
+    bool e = true;
+
+    cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+
+    return 0;
+}
